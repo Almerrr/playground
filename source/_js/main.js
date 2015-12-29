adde9b01bba194dde97b19dd2d45efc2552dd116
Almer Badart <almer.badart@clockwork.nl>
2015-12-29 20:48:18 +0100

    misc

--- source/_js/main.js
+++ source/_js/main.js
@@ -1,3 +1,15 @@
 $( document ).ready(function() {
-    
+	$('p.more').remove();
+	$('.toplink').remove();
+	$('a').attr('href', '#');
+
+	$('.theme').each(function(){
+		var nrTopics = $(this).find('li').length;
+		var newIt = nrTopics/4;
+		$(this).find('h3').append(newIt)
+	});
+
+
+
+	
 });
