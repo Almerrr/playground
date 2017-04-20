8d34454d31a4af03b8adb7b8798be23f3ef0469e
Almer Badart <almer.badart@clockwork.nl>
2017-04-20 23:20:36 +0200

    Clean Page function

--- gulpfile.js
+++ gulpfile.js
@@ -17,8 +17,7 @@ gulp.task('develop', function() {
     });
 
     gulp.watch(['build/pages/**/*.html']).on('change', function(file) {
-        // formatHtml(file);
-        gulp.start('clean-html-page');
+        formatHtml(file);
     });
 
     gulp.watch('source/_js/**/*').on('change', function(file) {
@@ -68,69 +67,35 @@ gulp.task('concat-js', function() {
 });
 
 var htmlbeautify = require('gulp-html-beautify');
+var beautifyOption = {
+    "indent_size": 4,
+    "indent_char": " ",
+    "eol": "\n",
+    "indent_level": 0,
+    "indent_with_tabs": false,
+    "preserve_newlines": false,
+    "max_preserve_newlines": 1,
+    "jslint_happy": false,
+    "space_after_anon_function": false,
+    "brace_style": "collapse",
+    "keep_array_indentation": true,
+    "keep_function_indentation": true,
+    "space_before_conditional": true,
+    "break_chained_methods": false,
+    "eval_code": false,
+    "unescape_strings": false,
+    "wrap_line_length": 0,
+    "wrap_attributes": "auto",
+    "wrap_attributes_indent_size": 4,
+    "end_with_newline": true
+};
 
 function formatHtml(file) {
     return gulp.src(file.path)
     .pipe(
-        htmlbeautify({
-            "indent_size": 4,
-            "indent_char": " ",
-            "eol": "\n",
-            "indent_level": 0,
-            "indent_with_tabs": false,
-            "preserve_newlines": false,
-            "max_preserve_newlines": 1,
-            "jslint_happy": false,
-            "space_after_anon_function": false,
-            "brace_style": "collapse",
-            "keep_array_indentation": true,
-            "keep_function_indentation": true,
-            "space_before_conditional": true,
-            "break_chained_methods": false,
-            "eval_code": false,
-            "unescape_strings": false,
-            "wrap_line_length": 0,
-            "wrap_attributes": "auto",
-            "wrap_attributes_indent_size": 4,
-            "end_with_newline": true
-        })
+        htmlbeautify(beautifyOption)
     )
     .pipe(
         gulp.dest('build/pages')
     );
 }
-
-
-gulp.task('clean-html-page', function(file) {
-    return gulp.src(['build/pages/**/*.html'])
-    .pipe(
-        changed('build/pages')
-    )
-    .pipe(
-        htmlbeautify({
-            "indent_size": 4,
-            "indent_char": " ",
-            "eol": "\n",
-            "indent_level": 0,
-            "indent_with_tabs": false,
-            "preserve_newlines": true,
-            "max_preserve_newlines": 1,
-            "jslint_happy": false,
-            "space_after_anon_function": false,
-            "brace_style": "collapse",
-            "keep_array_indentation": true,
-            "keep_function_indentation": true,
-            "space_before_conditional": true,
-            "break_chained_methods": false,
-            "eval_code": false,
-            "unescape_strings": false,
-            "wrap_line_length": 0,
-            "wrap_attributes": "auto",
-            "wrap_attributes_indent_size": 4,
-            "end_with_newline": true
-        })
-    )
-    .pipe(
-        gulp.dest('build/pages')
-    );
-});
