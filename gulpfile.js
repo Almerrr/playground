a7263a2ec87b6a6aab2a1ee3bb177cf59c46b33e
Almer Badart <almer.badart@clockwork.nl>
2017-04-20 22:57:39 +0200

    Cleaned html pages

--- gulpfile.js
+++ gulpfile.js
@@ -61,3 +61,36 @@ gulp.task('concat-js', function() {
     );
     return core;
 });
+
+var htmlbeautify = require('gulp-html-beautify');
+
+gulp.task('clean-html', function() {
+    return gulp.src(['build/pages/**/*.html'])
+    .pipe(
+        htmlbeautify({
+            "indent_size": 4,
+            "indent_char": " ",
+            "eol": "\n",
+            "indent_level": 0,
+            "indent_with_tabs": false,
+            "preserve_newlines": true,
+            "max_preserve_newlines": 1,
+            "jslint_happy": false,
+            "space_after_anon_function": false,
+            "brace_style": "collapse",
+            "keep_array_indentation": true,
+            "keep_function_indentation": true,
+            "space_before_conditional": true,
+            "break_chained_methods": false,
+            "eval_code": false,
+            "unescape_strings": false,
+            "wrap_line_length": 0,
+            "wrap_attributes": "auto",
+            "wrap_attributes_indent_size": 4,
+            "end_with_newline": true
+        })
+    )
+    .pipe(
+        gulp.dest('build/pages')
+    );
+});
